11a8a1aefffef433cbf1158d8a080379ad31f013
luyuanliang <luyuanliang_19842163.com>
2020-09-06 13:39:36 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # example-springcloud
 http://localhost:8080/testParam/twoParams?aa&dogAge=33&dogName=旺旺&studentName=xuza&homeddress=China&age=3&teacherList=222，灭霸&scores=33,44,55,66&studentDO2=
+
+http://localhost:8080/freemarker/sayFreeMarker
---
+++ src/main/java/org/web/example/springcloud/view/FreemarkerController.java
@@ -0,0 +1,25 @@
+package org.web.example.springcloud.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.web.example.springcloud.domain.StudentDO;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("freemarker")
+public class FreemarkerController {
+
+    @RequestMapping(value = "sayFreeMarker", method = {RequestMethod.POST, RequestMethod.GET})
+    public String sayFreeMark(Model model) {
+        model.addAttribute("account","freemarker");
+        StudentDO studentDO  = new StudentDO();
+        studentDO.setAge(2);
+        studentDO.setHomeaddress("China");
+        model.addAttribute("student",studentDO);
+        return "sayFreeMarker";
+    }
+}
