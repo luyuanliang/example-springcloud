d6192f743370630cb0d09a7d3fb5cba7a561616d
luyuanliang <luyuanliang_19842163.com>
2020-09-06 16:24:53 +0800

    Graallvm press test

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # example-springcloud
+验证参数
 http://localhost:8080/testParam/twoParams?aa&dogAge=33&dogName=旺旺&studentName=xuza&homeddress=China&age=3&teacherList=222，灭霸&scores=33,44,55,66&studentDO2=
 
+验证freemarker
 http://localhost:8080/freemarker/sayFreeMarker
+
+验证Graalvm
+
--- src/test/java/org/web/example/springcloud/TestGraalvmVMTest.java
+++ src/test/java/org/web/example/springcloud/TestGraalvmVMTest.java
@@ -6,7 +6,7 @@ import java.util.concurrent.*;
 
 public class TestGraalvmVMTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int count = 50000;
+        int count = 5000;
         int executeTimes = 1000;
         ExecutorService executorService = Executors.newFixedThreadPool(1000);
         List<Future<Integer>> list = new ArrayList<>();
@@ -27,5 +27,26 @@ public class TestGraalvmVMTest {
             }
         }
         System.out.println("end");
+    }
+
+}
+
+class ExecuteThread implements Callable {
+
+    private int count = 0;
+
+    public ExecuteThread(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        long current = System.currentTimeMillis();
+        Map map = new HashMap<Object, Object>();
+        for (int iter = 0; iter < count; iter++) {
+            map.put(count, UUID.randomUUID());
+        }
+        current = System.currentTimeMillis() - current;
+        return current;
     }
 }
