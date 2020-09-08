7f80e4e3e943a4b927218f8146f75de7d0e189c8
luyuanliang <luyuanliang_19842163.com>
2020-09-08 11:04:59 +0800

    Graallvm press test

--- src/main/java/org/web/example/springcloud/ExampleApplication.java
+++ src/main/java/org/web/example/springcloud/ExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = {"org.web.example.springcloud.dao"})
+//@MapperScan(basePackages = {"org.web.example.springcloud.dao"})
 public class ExampleApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/web/example/springcloud/dao/AccountDAO.java
+++ src/main/java/org/web/example/springcloud/dao/AccountDAO.java
@@ -1,9 +1,12 @@
 package org.web.example.springcloud.dao;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.web.example.springcloud.query.QueryAccount;
 import org.web.example.springcloud.domain.AccountDO;
 
+@Mapper
 public interface AccountDAO {
 
 	public AccountDO selectAccountByAccountId(Long accountId);
