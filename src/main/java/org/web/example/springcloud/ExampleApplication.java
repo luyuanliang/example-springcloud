f7b844cd1fbf9ceaaded183ecfdb980745d2f488
luyuanliang <luyuanliang_19842163.com>
2020-09-14 11:44:45 +0800

    datasource

--- src/main/java/org/web/example/springcloud/ExampleApplication.java
+++ src/main/java/org/web/example/springcloud/ExampleApplication.java
@@ -3,9 +3,13 @@ package org.web.example.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication()
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 //@MapperScan(basePackages = {"org.web.example.springcloud.dao"})
+@ImportResource({"classpath:mapper/mybatis-config-all.xml"})
 public class ExampleApplication {
 
     public static void main(String[] args) {
