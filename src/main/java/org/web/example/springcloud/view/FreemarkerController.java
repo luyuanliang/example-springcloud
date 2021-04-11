6d1df85b9ef2db05f576574726e6b9cf1effa692
luyuanliang <luyuanliang_19842163.com>
2021-04-11 16:21:35 +0800

    update

--- src/main/java/org/web/example/springcloud/view/FreemarkerController.java
+++ src/main/java/org/web/example/springcloud/view/FreemarkerController.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.web.example.springcloud.domain.StudentDO;
-
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("freemarker")
